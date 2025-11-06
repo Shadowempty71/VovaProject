31c82cfcc825a2e1794f656d803fea8a4678620b
Shadowempty71 <shadowempty71@gmail.com>
2025-11-06 16:26:36 +0300

    Реализован калькулятор

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Привет, мир!</p>"
+    return "Привет, мир!"
 
+@app.route("/calc/<operator>")
+def calc(operator):
+    nums = request.args.get("nums")
+    nums_array = nums.split(',')
+    x, y = nums_array[0], nums_array[1]
+    x,y = int(x),int(y)
+
+    if operator == "plus":
+        res = x + y
+        return str(res)
+    elif operator == "minus":
+        res = x - y
+        return x - y
+    else:
+        return "Error!"
